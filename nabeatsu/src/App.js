b0afb1c5787c15e54e25eb0f025291e7d4f48598
zap-k-namikawa <k_namikawa@zappallas.com>
2017-10-31 16:00:19 +0900

    2ndDay

--- nabeatsu/src/App.js
+++ nabeatsu/src/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
   }
+
+  handleClicks() {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
+  handleClick() {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+
+
+
+
+
+  render(){
+    return(
+      <div>
+        <div class="display">{this.state.count}</div>
+        <button onClick={this.handleClicks.bind(this)}>+</button>
+        <button onClick={this.handleClick.bind(this)}>-</button>
+      </div>
+    )
+  }
+
+
+
+  
 }
 
 export default App;
