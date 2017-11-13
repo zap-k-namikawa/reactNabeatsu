b3a07649c1dcb1c200fc13e133d1dd0c568c1ab8
zap-k-namikawa <k_namikawa@zappallas.com>
2017-11-13 12:54:49 +0900

    image

--- nabeatsu/src/App.js
+++ nabeatsu/src/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+// import logo from 'img/logo.svg';
 import './App.css';
-// import ClassNames from 'classnames';
 
-const styles = {
-  normal: {
-  },
-  baisu: {
-    backgroundColor: 'red',
-  }
-};
+
+const baisu = 'https://www.google.co.jp/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
+
 
 class App extends Component {
   constructor() {
@@ -35,7 +30,7 @@ class App extends Component {
   render(){
     return(
       <div className="block">
-        <div className="display" style={this.getIsSan()}>{this.state.count}</div>
+        <div className="display"><img src={this.getIsSan()} />{this.state.count}</div>
         <div className="button">
         <button className="btn btnPlus" onClick={this.handleClicks.bind(this)}>+</button>
         <button className="btn btnMinus"onClick={this.handleClick.bind(this)}>-</button>
@@ -45,7 +40,7 @@ class App extends Component {
   }
 
   getIsSan() {
-    return this.state.count % 3 === 0 && this.state.count !== 0 ? styles.baisu : styles.normal ;
+    return this.state.count % 3 === 0 && this.state.count !== 0 ? baisu : null ;
   }
 
 
