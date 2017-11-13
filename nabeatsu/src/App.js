56ff69d5d36c8d245351507211d9b3d21796343b
zap-k-namikawa <k_namikawa@zappallas.com>
2017-11-13 11:57:12 +0900

    background_ok

--- nabeatsu/src/App.js
+++ nabeatsu/src/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+// import ClassNames from 'classnames';
+
+const styles = {
+  normal: {
+  },
+  baisu: {
+    backgroundColor: 'red',
+  }
+};
 
 class App extends Component {
   constructor() {
@@ -23,18 +32,23 @@ class App extends Component {
   }
 
 
-
-
-
   render(){
     return(
-      <div>
-        <div class="display">{this.state.count}</div>
-        <button onClick={this.handleClicks.bind(this)}>+</button>
-        <button onClick={this.handleClick.bind(this)}>-</button>
+      <div className="block">
+        <div className="display" style={this.getIsSan()}>{this.state.count}</div>
+        <div className="button">
+        <button className="btn btnPlus" onClick={this.handleClicks.bind(this)}>+</button>
+        <button className="btn btnMinus"onClick={this.handleClick.bind(this)}>-</button>
+        </div>
       </div>
     )
   }
+
+  getIsSan() {
+    return this.state.count % 3 === 0 && this.state.count !== 0 ? styles.baisu : styles.normal ;
+  }
+
+
 
 
 
